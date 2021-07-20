f28c963b8d3fe7f75639b4cd0920f59037403a1f
Amal4m41 <amalsunil117@gmail.com>
2021-07-20 21:38:50 +0530

    Updated examples which can be used for projects

--- CoroutineExamples/app/src/main/java/com/example/coroutineexamples/MainActivity3.kt
+++ CoroutineExamples/app/src/main/java/com/example/coroutineexamples/MainActivity3.kt
@@ -3,6 +3,9 @@ package com.example.coroutineexamples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 
 class MainActivity3 : AppCompatActivity() {
@@ -11,6 +14,28 @@ class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        val button=findViewById<Button>(R.id.Button)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "CLICKED BUTTON", Toast.LENGTH_SHORT).show()
+
+//            runBlocking {
+//                Log.e(TAG, "Thread : ${Thread.currentThread().name}") //main thread
+//                val job=async {networkCall()}
+//                val stringValue=job.await()  //will block the main thread until this result it obtained(therefore blocks
+//                //the UI update )
+//                Log.e(TAG, stringValue )
+//            }
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                //CoroutineScope tied to this LifecycleOwner's Lifecycle.
+                //This scope will be cancelled when the Lifecycle is destroyed
+                Log.e(TAG, "Thread : ${Thread.currentThread().name}") //main thread
+                val job=async {networkCall()}
+                val stringValue=job.await()
+                Log.e(TAG, stringValue )
+            }
+        }
 
 /*
         Log.e(TAG, "Before Coroutine : ${Thread.currentThread().name}", )
@@ -28,40 +53,41 @@ class MainActivity3 : AppCompatActivity() {
         Log.e(TAG, "After Coroutine : ${Thread.currentThread().name}", )
 */
 
-        Log.e(TAG, "Before runBlocking" )
-        runBlocking {
-//            launch {
-//                Log.e(TAG, "Launch: ${Thread.currentThread().name}", )  //main thread(but non blocking)
+//        Log.e(TAG, "Before runBlocking" )
+//        runBlocking {
+////            launch {
+////                Log.e(TAG, "Launch: ${Thread.currentThread().name}", )  //main thread(but non blocking)
+////            }
+//            launch(Dispatchers.IO) {
+//                delay(3000L)
+//                Log.e(TAG, "Launch 1 finished")
 //            }
-            launch(Dispatchers.IO) {
-                delay(3000L)
-                Log.e(TAG, "Launch 1 finished")
-            }
-            launch(Dispatchers.IO) {
-                delay(6000L)
-                Log.e(TAG, "Launch 2 finished")
-            }
+//            launch(Dispatchers.IO) {
+//                delay(6000L)
+//                Log.e(TAG, "Launch 2 finished")
+//            }
+//
+//            Log.e(TAG, "Inside runBlocking")
+////            delay(5000L)
+//            Log.e(TAG, "Inside runBlocking2")
+//            //After executing this line, runBlocking waits for the launch 1 and launch 2 coroutines to get over.
+//        }
+//        Log.e(TAG, "After runBlocking" )
+//        /*
+//2021-07-20 19:13:29.834 1772-1772/com.example.coroutineexamples E/Check: Before runBlocking
+//2021-07-20 19:13:29.962 1772-1772/com.example.coroutineexamples E/Check: Inside runBlocking
+//2021-07-20 19:13:29.963 1772-1772/com.example.coroutineexamples E/Check: Inside runBlocking2
+//2021-07-20 19:13:32.971 1772-1822/com.example.coroutineexamples E/Check: Launch 1 finished
+//2021-07-20 19:13:35.970 1772-1822/com.example.coroutineexamples E/Check: Launch 2 finished
+//2021-07-20 19:13:35.972 1772-1772/com.example.coroutineexamples E/Check: After runBlocking
+//         */
 
-            Log.e(TAG, "Inside runBlocking")
-//            delay(5000L)
-            Log.e(TAG, "Inside runBlocking2")
-            //After executing this line, runBlocking waits for the launch 1 and launch 2 coroutines to get over.
-        }
-        Log.e(TAG, "After runBlocking" )
-        /*
-2021-07-20 19:13:29.834 1772-1772/com.example.coroutineexamples E/Check: Before runBlocking
-2021-07-20 19:13:29.962 1772-1772/com.example.coroutineexamples E/Check: Inside runBlocking
-2021-07-20 19:13:29.963 1772-1772/com.example.coroutineexamples E/Check: Inside runBlocking2
-2021-07-20 19:13:32.971 1772-1822/com.example.coroutineexamples E/Check: Launch 1 finished
-2021-07-20 19:13:35.970 1772-1822/com.example.coroutineexamples E/Check: Launch 2 finished
-2021-07-20 19:13:35.972 1772-1772/com.example.coroutineexamples E/Check: After runBlocking
-         */
 
 
     }
     suspend fun networkCall():String{
-        delay(5000)
-        Log.e(TAG, "networkCall: ", )
+        delay(10000L)
+        Log.e(TAG, "complete fake networkCall")
         return "Result 1"
     }
 }
