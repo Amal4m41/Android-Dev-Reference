5717cee17c0f1e51269aa1493e3e96568a892d5f
Amal4m41 <amalsunil117@gmail.com>
2021-09-12 23:25:44 +0530

    Update summarizer code with delimiter and async method

--- TextSummarizerDemo/app/src/main/java/com/example/textsummarizerdemo/MainActivity.kt
+++ TextSummarizerDemo/app/src/main/java/com/example/textsummarizerdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.textsummarizerdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.example.textsummarizerdemo.databinding.ActivityMainBinding
 import com.ml.quaterion.text2summary.Text2Summary
 
@@ -20,11 +21,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    val callback = object : Text2Summary.SummaryCallback {
+        override fun onSummaryProduced(summary: String) {
+            // The summary is ready!
+            Log.i("RESULT", "Summary: ${summary.split("\\.\\s*\\.".toRegex())}")
+//            Log.i("RESULT", "Summary: $summary")
+            binding.progressBar.visibility=View.INVISIBLE
+        }
+    }
+
     private fun getTextSummary(){
+        binding.progressBar.visibility= View.VISIBLE
         val text = binding.etTextInput.text.toString()
 
-        val summary =  Text2Summary.summarize(text, compressionRate = 0.7f )
-
-        Log.i("RESULT", "Summary: $summary")
+//        val summary =  Text2Summary.summarize(text, compressionRate = 0.7f )  //Will pause the main thread during heavy jobs
+        Text2Summary.summarizeAsync( text , 0.7f , callback  )
     }
 }
