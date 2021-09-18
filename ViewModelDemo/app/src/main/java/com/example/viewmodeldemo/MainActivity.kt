c2a0c3e35f40efb1f5aa224aca06044dfdd51d54
Amal4m41 <amalsunil117@gmail.com>
2021-09-18 14:00:01 +0530

    updated code w.r.t livedata explanation

--- ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
+++ ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.viewmodeldemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
@@ -14,15 +17,44 @@ class MainActivity : AppCompatActivity() {
 
 
         val viewModelProviderFactory = RandomNumberViewModelFactory()
+
+        //Creates ViewModelProvider, which will create ViewModels via the given Factory and retain them in a store of the given ViewModelStoreOwner
+        //In this case the ViewModelStoreOwner is this activity(this).
         val viewModel = ViewModelProvider(this,viewModelProviderFactory).get(MyRandomNumberGenerator::class.java)
 
 //        findViewById<TextView>(R.id.tvRandomNumber).text =
 //                "Random Number : ${MyRandomNumberGenerator().getNumber()}"
-        findViewById<TextView>(R.id.tvRandomNumber).text =
-                "Random Number : ${viewModel.getNumber()}"
+//        findViewById<TextView>(R.id.tvRandomNumber).text =
+//                "Random Number : ${viewModel.getNumber()}"
+
+        /*
+        Adds the given observer to the observers list within the lifespan of the given owner. The events are dispatched on the main thread. If LiveData already has data set, it will be delivered to the observer.
+The observer will only receive events if the owner is in Lifecycle.State.STARTED or Lifecycle.State.RESUMED state (active).
+If the owner moves to the Lifecycle.State.DESTROYED state, the observer will automatically be removed.
+When data changes while the owner is not active, it will not receive any updates. If it becomes active again, it will receive the last available data automatically.
+LiveData keeps a strong reference to the observer and the owner as long as the given LifecycleOwner is not destroyed. When it is destroyed, LiveData removes references to the observer & the owner.
+If the given owner is already in Lifecycle.State.DESTROYED state, LiveData ignores the call.
+If the given owner, observer tuple is already in the list, the call is ignored. If the observer is already in the list with another owner, LiveData throws an IllegalArgumentException.
+         */
+
+        val livedata:LiveData<String> = viewModel.getNumber()
+
+        Log.e("TAG", "onCreate: $livedata", )
+        livedata.observe(this, object : Observer<String> {
+            override fun onChanged(t: String?) {
+                //Called when the data changes, or if the livedata already has some data it'll be delivered here.
+                Log.e("TAG", "onChanged: ", )
+                findViewById<TextView>(R.id.tvRandomNumber).text = t!!
+            }
+        })
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Log.e("TAG", "BUTTON FETCH CLICKED ", )
+            viewModel.generateRandomNumber()
+        }
 
 
 
-        Log.i("TAG", "onCreate: ")
+//        Log.e("TAG", "onCreate: ")
     }
 }
--- ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MyRandomNumberGenerator.kt
+++ ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MyRandomNumberGenerator.kt
@@ -1,33 +1,44 @@
 package com.example.viewmodeldemo
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.*
 
 class MyRandomNumberGenerator: ViewModel() {
 
-    private var randomNumber: String? = null
-
-    public fun getNumber():String{
-        //if it's not null then return the random number
+    private val randomNumber: MutableLiveData<String> = MutableLiveData()
+//    public var randomNumber: LiveData<String>? = null
 
 
-
-        Log.i("TAG", "getNumber: ")
-        if(randomNumber==null){
-            randomNumber=generateRandomNumber()
-        }
-        return randomNumber!!
+    init {
+//        getNumber()
     }
 
-    private fun generateRandomNumber():String{
-        Log.i("TAG", "generateRandomNumber: ")
-        return (Random().nextInt(10)+1).toString()
+
+    public fun getNumber():LiveData<String>{
+
+//        Log.e("TAG", "getNumber: ")
+//        randomNumber.postValue((Random().nextInt(10)+1).toString())
+        if(randomNumber.value.isNullOrEmpty()) {
+            generateRandomNumber()
+        }
+        return randomNumber
+    }
+
+    public fun generateRandomNumber() {
+//        Log.e("TAG", "generateRandomNumber: ")
+//        randomNumber.postValue((Random().nextInt(10)+1).toString())
+        val str = (Random().nextInt(10) + 1).toString()
+        randomNumber.postValue(str)
+
+        Log.e("TAG", "generateRandomNumber: $randomNumber", )
     }
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("TAG", "onCleared: ")
+        Log.e("TAG", "onCleared: ")
     }
 }
 
